4b8bc4fc33eef26095bc7b2ab60d883ad303aa53
Darah Snider <darahsnider@gmail.com>
2021-11-13 15:50:17 -0500

    after sidebar module 1.3

---
+++ src/components/site/Sidebar.js
@@ -0,0 +1,31 @@
+import { getDefaultNormalizer } from '@testing-library/dom';
+import {
+    Route,
+    Link,
+    Switch
+} from 'react-router-dom';
+import Home from './Home';
+import Resources from './Resources';
+
+const Sidebar = () => {
+    return(
+        <div className='sidebar'>
+            <div className='sidebar-list-styling'>
+                <ul className='sidebar-list list-unstyled'>
+                    <li><Link to='/'>Home</Link></li>
+                    <li><Link to='/Resources'>Resources</Link></li>
+                    <li><Link to='/functionalcomponent'>Functional Component</Link></li>
+                </ul>
+            </div>
+            <div className='sidebar-route'>
+                <Switch>
+                    <Route exact path='/home'>Home<Home /></Route>
+                    <Route exact path='/resources'><Resources /></Route>
+                    <Route exact path='/'><Home /></Route>
+                </Switch>
+            </div>
+        </div>
+    );
+};
+
+export default Sidebar;
