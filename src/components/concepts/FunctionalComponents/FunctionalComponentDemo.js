39a7b700d78c8e25e014c3b9523d8809903434e0
Darah Snider <darahsnider@gmail.com>
2021-11-14 20:36:14 -0500

    challenge 2.5 complete, working react

--- src/components/concepts/FunctionalComponents/FunctionalComponentDemo.js
+++ src/components/concepts/FunctionalComponents/FunctionalComponentDemo.js
@@ -3,10 +3,21 @@
           <div className='main'>
               <div className='mainDiv'>
                   <div>
-                  Hello React
+                 <h1>Functional Component</h1>
+                 <p>Functional Components are the primary tool in React to build a small, modular piece of your page.</p>
               </div>
               <div>
-                  How are you today?
+                  <dl>
+                      <dt><b>Can use state</b></dt>
+                      <dd>With the 'useState' hook, functional components can now both render a display to the page and update the information to be shown.</dd>
+                      <dt><b>No 'this' keyword</b></dt>
+                      <dd>Older class components use 'this', functional components have no 'this' object.</dd>
+                      <dt><b>Can use effects</b></dt>
+                      <dd>With the 'useEffect hook, functional components can perform side effect with any props of state changes.</dd>
+                      <dt><b>return()</b></dt>
+                      <dd>Must return a single element, but this element may have nested elements inside.</dd>
+                  </dl>
+                  <h2>Functional Syntax versus Arrow Function</h2>
                 </div>
               </div>
           </div>
