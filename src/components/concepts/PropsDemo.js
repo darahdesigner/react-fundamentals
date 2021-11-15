fcdb2613cac3cf4e79c085774178cb5df68bb0e0
Darah Snider <darahsnider@gmail.com>
2021-11-15 00:46:48 -0500

    props challenge 6.4 complete

--- src/components/concepts/PropsDemo.js
+++ src/components/concepts/PropsDemo.js
@@ -1,14 +1,50 @@
 import {useState} from 'react';
 
 const PropsDemo = () => {
+    const [ color, setColor ] = useState('white');
+    const [ backgroundColor, setBackgroundColor ] = useState('purple');
+    const [ borderRadius, setBorderRadius] = useState('5px');
+    const [ borderStyle, setBorderStyle ] = useState('dashed');
+    const [ display, setDisplay ] =useState('inline-block');
+    const [ width, setWidth ] = useState('350px');
+    const [ textAlign, setTextAlign ] = useState('center');
+
+    let styles = {
+        color : color,
+        backgroundColor : backgroundColor,
+        borderRadius : borderRadius,
+        borderStyle : borderStyle,
+        display : display,
+        width : width,
+        textAlign : textAlign,
+    }
+
+        const toggleColor = () => {
+            color === 'white' ? setColor('pink') : setColor('white');
+    }
+
+        const toggleBackgroundColor = () => {
+            backgroundColor === 'purple' ? setBackgroundColor('black') : setBackgroundColor('purple');
+        }
+
+        const toggleBorderRadius = () => {
+            borderRadius === '5px' ? setBorderRadius ('20px') : setBorderRadius('5px');
+        }
+
+        const toggleBorderStyle = () => {
+            borderStyle == 'dashed' ? setBorderStyle('double') : setBorderStyle('dashed');
+        }
+
     return(
         <div className='main'>
             <div className='mainDiv'>
-                <FunctionalComponent string='will this display?' />
-                <FunctionalComponent string='props are pretty cool, right?' />
-                <FunctionalComponent string='you can send data from one component...' />
-                <FunctionalComponent string='...to another!' />
+                <div style={styles}>
+                <FunctionalComponent string='will this display?' function={toggleColor} />
+                <FunctionalComponent string='props are pretty cool, right?' function={toggleBackgroundColor} />
+                <FunctionalComponent string='you can send data from one component...' function={toggleBorderRadius} />
+                <FunctionalComponent string='...to another!' function={toggleBorderStyle} />
             </div>
+        </div>
         </div>
     );
 };
@@ -19,6 +55,7 @@ const FunctionalComponent = (props) => {
     return(
         <div>
             <p>{props.string}</p>
+            <button onClick={props.function}>Press Me!</button>
         </div>
     );
 };
