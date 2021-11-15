45b9898c6f1b3a9dff8e013dbeb5af2bcca7b3b2
Darah Snider <darahsnider@gmail.com>
2021-11-15 00:18:47 -0500

    Effects Challenge 5.4 complete

--- src/components/site/Sidebar.js
+++ src/components/site/Sidebar.js
@@ -9,6 +9,7 @@ import Resources from './Resources';
 import FunctionalComponentDemo from '../concepts/FunctionalComponents/FunctionalComponentDemo';
 import JSXRules from '../concepts/JSXRules';
 import State from '../concepts/States'
+import Effects from '../concepts/Effects';
 
 const Sidebar = () => {
     return(
@@ -20,6 +21,7 @@ const Sidebar = () => {
                     <li><Link to='/functionalcomponent'>Functional Component</Link></li>
                     <li><Link to='/jsxrules'>JSX Rules</Link></li>
                     <li><Link to='/state'>State</Link></li>
+                    <li><Link to='/effects'>Effects</Link></li>
                 </ul>
             </div>
             <div className='sidebar-route'>
@@ -30,6 +32,7 @@ const Sidebar = () => {
                     <Route exact path='/functionalcomponent'><FunctionalComponentDemo /></Route>
                     <Route exact path='/jsxrules'><JSXRules /></Route>
                     <Route exact path='/state'><State /></Route>
+                    <Route exact path='/effects'><Effects /></Route>
                 </Switch>
             </div>
         </div>
