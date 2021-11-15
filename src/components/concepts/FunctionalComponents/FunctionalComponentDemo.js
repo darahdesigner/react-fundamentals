2dfd9e3182b478585303ba853d8d66c7532a630b
Darah Snider <darahsnider@gmail.com>
2021-11-14 21:01:34 -0500

    challenge 2.7 complete, working react

--- src/components/concepts/FunctionalComponents/FunctionalComponentDemo.js
+++ src/components/concepts/FunctionalComponents/FunctionalComponentDemo.js
@@ -1,27 +1,77 @@
+  import {
+      Card,
+      CardText,
+      CardBody,
+      CardTitle,
+      CardSubtitle,
+      Button,
+      Container, 
+      Row,
+      Col
+  } from 'reactstrap';
+  
   const FunctionalComponentDemo = () => {
       return(
-          <div className='main'>
-              <div className='mainDiv'>
-                  <div>
-                 <h1>Functional Component</h1>
-                 <p>Functional Components are the primary tool in React to build a small, modular piece of your page.</p>
-              </div>
-              <div>
+          <Container className='main'>
+              <Row>
+                  <Col xs='12'>
+                  <h1>Functional Component</h1>
+                  <p>Functional Components are the primary tool in React to build a small, modular piece of your page.</p>
                   <dl>
-                      <dt><b>Can use state</b></dt>
+                      <dt>Can use state</dt>
                       <dd>With the 'useState' hook, functional components can now both render a display to the page and update the information to be shown.</dd>
-                      <dt><b>No 'this' keyword</b></dt>
+                      <dt>No 'this' keyword</dt>
                       <dd>Older class components use 'this', functional components have no 'this' object.</dd>
-                      <dt><b>Can use effects</b></dt>
+                      <dt>Can use effects</dt>
                       <dd>With the 'useEffect hook, functional components can perform side effect with any props of state changes.</dd>
-                      <dt><b>return()</b></dt>
+                      <dt>return()</dt>
                       <dd>Must return a single element, but this element may have nested elements inside.</dd>
                   </dl>
-                  <h2>Functional Syntax versus Arrow Function</h2>
-                </div>
-              </div>
-          </div>
+                  </Col>
+              </Row>
+              <hr />
+              <h1>Challenge 1</h1>
+                  <Row>
+                      <Col md='6'>
+                          <HellowWorldFatArrow className='logo' />
+                      </Col>
+                      <Col md='6'>
+                          <HelloWorld />
+                      </Col>
+                  </Row>
+            </Container>
       );
   };
 
   export default FunctionalComponentDemo;
+
+  const HelloWorld = function() {
+      return(
+          <div>
+              <Card>
+                  <img width='100%' height='280px' src = 'https://i.ytimg.com/vi/BwAakF_VUV8/maxresdefault.jpg' alt='card cap' />
+                  <CardBody>
+                      <CardTitle>Regular Ol' Function</CardTitle>
+                      <CardSubtitle> A JS Library</CardSubtitle>
+                      <CardText><pre>const HelloWorld = function()</pre></CardText>
+                      <Button>Go somewhere, yo</Button>
+                  </CardBody>
+              </Card>
+          </div>
+      );
+  };
+
+  //fat arrow function componenet - 3 fewer lines. Common in React...
+  const HellowWorldFatArrow = () =>
+  <div>
+      <Card>
+          <img width='100%' height='280px' src='https://i.ytimg.com/vi/_pfXEv9cFGE/maxresdefault.jpg' alt='card cap' />
+          <CardBody>
+          <CardTitle>Fat Arrow</CardTitle>
+          <CardSubtitle>A JS Library</CardSubtitle>
+          <CardText><pre>const HelloWorld = () =></pre></CardText> 
+          <Button>Go somewhere, man</Button>
+          </CardBody>
+      </Card>
+  </div>
+  //ignore the problem on line 72 :)
