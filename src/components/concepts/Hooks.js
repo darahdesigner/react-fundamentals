811a658cb267d67ba2c5143b47815e610581e617
Darah Snider <darahsnider@gmail.com>
2021-11-15 23:44:32 -0500

    Hook challenge 7.3 complete

--- src/components/concepts/Hooks.js
+++ src/components/concepts/Hooks.js
@@ -1,8 +1,9 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 
 const Hooks = () => {
     const [query, setQuery] = useState('');
     const [results, setResults] = useState({});
+
     const fetcher = () => {
         fetch(`https://swapi.dev/api/people/${query}`)
             .then(res => res.json())
@@ -11,6 +12,9 @@ const Hooks = () => {
                 setResults(json);
             })
     }
+    
+    
+
     return(
         <div className="main">
             <div className="mainDiv">
@@ -22,4 +26,49 @@ const Hooks = () => {
     )
 }
 
-export default Hooks;
+const Hooks2 = () => {
+    const [results, queryNum, setQueryNum] = useNumHook('');
+    const [clicks, setClicks] = useClicks(0);
+
+    return(
+        <div className="main">
+            <div className="mainDiv">
+                <h3>Enter a number below to see a number fact</h3>
+                <input value={queryNum} onChange={e => setQueryNum(e.target.value)} placeholder="enter a number"/>
+                {<button onClick={() => setClicks(clicks + 1)}>Click to update document title</button> }
+                {results ? <h2>{results}</h2> : <div></div>}
+            </div>
+        </div>
+    );
+};
+
+
+const useNumHook = (num) => {
+    const [queryNum, setQueryNum] = useState(num);
+    const [results, setResults] = useState('');
+
+    useEffect(() => {
+        if (queryNum !=='') {
+            fetch(`http://numbersapi.com/${queryNum}`)
+            .then(res => res.text())
+            .then(json => {
+                console.log(json);
+                setResults(json);
+            });
+        };
+    }, [queryNum]);
+
+    return [results, queryNum, setQueryNum]
+};
+
+const useClicks = (initCount) => {
+    const [clicks, setClicks] = useState(initCount);
+
+    useEffect(() => {
+        document.title = `You have clicked ${clicks} times`;
+    }, [clicks])
+    return [clicks, setClicks]
+}
+
+export default Hooks2;
+
